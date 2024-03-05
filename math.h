38bde2cd529b2175dedd31829f2319f7920a67e2
Clara Lille <the@foxx.at>
2024-03-05 04:56:08 +0000

    Add basic project files

---
+++ math.h
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  DsMath Source File.
+//  Copyright (C) 2024 Clara Lille
+// -------------------------------------------------------------------------
+//  File name:   math.h
+//  Version:     v1.00
+//  Created:     05/03/24 by Clara.
+//  Description: 
+// -------------------------------------------------------------------------
+//  This project is licensed under the MIT License
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include "dsmath/mat4.ispc.h"
+
+#ifndef DSMATH_MATH_H
+#define DSMATH_MATH_H
+
+
+#endif
