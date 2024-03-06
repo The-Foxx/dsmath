ea2366e0b47139dbf560323df4d6af1b8b68a848
Clara Lille <the@foxx.at>
2024-03-06 02:51:51 +0000

    Turn test info c++ instad of c

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 //  QuestDS Engine Source File.
 //  Copyright (C) 2024 Clara Lille
 // -------------------------------------------------------------------------
-//  File name:   test.c
+//  File name:   test.cpp
 //  Version:     v1.00
 //  Created:     06/03/24 by Clara.
 //  Description: 
@@ -17,10 +17,18 @@
 
 int main(int argc, char* argv[]){
 	dsvec3 TestAddVec1;
-	dsvec3 TestAddVec2 { 3.0F, 5.0F, 1.5F };
-	dsvec3 TestAddVec3 { 4.0F, 15.0F, 6.9F };
-	TestAddVec1 ds_vec3add(TestAddVec2, TestAddVec3);
+	dsvec3 TestAddVec2;
+	dsvec3 TestAddVec3;
+	TestAddVec2.X = 3.0F;
+	TestAddVec2.Y = 5.5F;
+	TestAddVec2.Z = 15.0F;
+	TestAddVec3.X = 7.0F;
+	TestAddVec3.Y = 6.9F;
+	TestAddVec3.Z = 1.0F;
+
+	TestAddVec1 = ds_vec3add(TestAddVec2, TestAddVec3);
 
 	return 0;
 
 }
+
